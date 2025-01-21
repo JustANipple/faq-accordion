0d5fab687b99733d7562cbfb72baf63f9a597d14
JustANipple <prandisamuel99@gmail.com>
2025-01-21 12:50:39 +0100

    fixed an accessibility error with duplicated id

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function toggleAnswer(button) {
   const parentContainer = button.parentElement.parentElement;
-  const answer = parentContainer.querySelector("#answer");
+  const answer = parentContainer.querySelector(".text-e_grayishPurple");
   const image = parentContainer.querySelector("img");
   answer.classList.toggle("hidden");
   if (answer.classList.contains("hidden")) {
